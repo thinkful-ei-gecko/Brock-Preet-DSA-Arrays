c42e2854f2199367eb919267520cdff099bf5c49
Brock Boutwell <brockboutwell@gmail.com>
2019-11-06 16:01:37 -0500

    9 and 10 done

--- drills.js
+++ drills.js
@@ -1,12 +1,11 @@
-'use strict';
-
+"use strict";
 
 //Q5: URLify a string
 function urlify(str) {
-  let newStr = str.split(' ').join('%20');
+  let newStr = str.split(" ").join("%20");
   //console.log(newStr);
 }
-urlify('www.thinkful.com         /tauh ida parv een');
+urlify("www.thinkful.com         /tauh ida parv een");
 //time complexity - O(1)
 
 //Q6: Filtering an array
@@ -20,12 +19,13 @@ urlify('www.thinkful.com         /tauh ida parv een');
 //time complexity - O(n)
 
 //Q7: Max sum in the array
-function maxSum(arr){
+function maxSum(arr) {
   let currentSum = 0; //O(1)
   let maxSum = 0; //O(1)
-  for(let i = 0; i < arr.length; i++){ //O(n)
+  for (let i = 0; i < arr.length; i++) {
+    //O(n)
     currentSum += arr[i];
-    if(maxSum < currentSum){
+    if (maxSum < currentSum) {
       maxSum = currentSum;
     }
   }
@@ -35,11 +35,43 @@ function maxSum(arr){
 //O(n) - linear time complexity
 
 //Q8: Merge arrays
-function mergeArray(arr1, arr2){
+function mergeArray(arr1, arr2) {
   let result = [...arr1, ...arr2];
-  return result.sort((a,b) => a - b);
+  return result.sort((a, b) => a - b);
 }
 console.log(mergeArray([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
 //O(n log n) - sorting
 
 //Q9: Remove characters
+function removeChar(str) {
+  let newStr = str.replace(/[aeiou]/gi, "");
+  return newStr;
+}
+let originalStr = "this is a string to remove vowels from";
+removeChar(originalStr);
+// O(n)
+
+//Q10: Products
+function product(arr) {
+  let result = [];
+
+  let productSoFar = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = productSoFar;
+    productSoFar *= arr[i];
+  }
+  console.log(productSoFar);
+
+  productSoFar = 1;
+
+  for (let j = arr.length - 1; j >= 0; j--) {
+    result[j] *= productSoFar;
+    productSoFar *= arr[j];
+  }
+
+  return result;
+}
+var array = [1, 3, 9, 4];
+product(array);
+// O(n) 
