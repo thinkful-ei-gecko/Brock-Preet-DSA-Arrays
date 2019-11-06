0839ffe8fd886a2abd0d41d47b4e8d993479a27b
preet-singh <preetsingh13@gmail.com>
2019-11-06 15:56:12 -0600

    Q11 in progress

--- drills.js
+++ drills.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
 //Q5: URLify a string
 function urlify(str) {
-  let newStr = str.split(" ").join("%20");
+  let newStr = str.split(' ').join('%20');
   //console.log(newStr);
 }
-urlify("www.thinkful.com         /tauh ida parv een");
+urlify('www.thinkful.com         /tauh ida parv een');
 //time complexity - O(1)
 
 //Q6: Filtering an array
@@ -39,15 +39,15 @@ function mergeArray(arr1, arr2) {
   let result = [...arr1, ...arr2];
   return result.sort((a, b) => a - b);
 }
-console.log(mergeArray([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
+// console.log(mergeArray([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
 //O(n log n) - sorting
 
 //Q9: Remove characters
 function removeChar(str) {
-  let newStr = str.replace(/[aeiou]/gi, "");
+  let newStr = str.replace(/[aeiou]/gi, '');
   return newStr;
 }
-let originalStr = "this is a string to remove vowels from";
+let originalStr = 'this is a string to remove vowels from';
 removeChar(originalStr);
 // O(n)
 
@@ -61,7 +61,7 @@ function product(arr) {
     result[i] = productSoFar;
     productSoFar *= arr[i];
   }
-  console.log(productSoFar);
+  // console.log(productSoFar);
 
   productSoFar = 1;
 
@@ -75,3 +75,59 @@ function product(arr) {
 var array = [1, 3, 9, 4];
 product(array);
 // O(n) 
+
+
+//Q11: 2D array
+
+function zeroArray(arr){
+  let column = [];
+  let row = [];
+
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; i < arr.length; i++){
+      if(arr[i][j] === 0){
+        column[j] = true;
+        row[i] = true;
+      }
+    }
+  }
+  for(let i = 0; i < arr.length; i++){
+    let rows = arr[i];
+    for (let j = 0; j < rows.length; j++){
+      //let columns = rows[j];
+      if(column[j] || row[i]){
+        rows[j] = 0;
+      }
+    }
+  }
+  // for(let i = 0; i < row.length; i++){
+  //   for(let j = 0; j < arr[row].length; j++){
+  //     arr[row[i][j]] = 0;
+  //   }
+  // }
+
+  // for(let i = 0; i < column.length; i++){
+  //   for(let j = 0; j < arr[column].length; j++){
+  //     arr[j][column[i]] = 0;
+  //   }
+  // }
+  return arr;
+}
+console.log(zeroArray([[[1,0,1,1,0],
+  [0,1,1,1,0],
+  [1,1,1,1,1],
+  [1,0,1,1,1],
+  [1,1,1,1,1]]]));
+
+
+//Q12: String Rotation
+
+function stringRotation(str1, str2){
+  if(str1.length !== str2.length){
+    return false;
+  }
+  let string = str1 + str1;
+  return string.includes(str2);
+}
+console.log(stringRotation('amazon', 'azonma'));
+console.log(stringRotation('amazon', 'azonam'));
